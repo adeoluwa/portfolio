c2871aca69123fe2145c9146ad5aace7bd6bbba4
Kiishi_joseph <kiishijoseph@gmail.com>
2022-11-08 13:33:54 +0100

    updated contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,12 +26,14 @@ const Contact = () => {
               name="name"
               placeholder="Enter Your Name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              required
             />
             <input
               type="email"
               name="email"
               placeholder="Enter Your Email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              required
             />
 
             <textarea
@@ -39,6 +41,7 @@ const Contact = () => {
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              required
             ></textarea>
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's talk
